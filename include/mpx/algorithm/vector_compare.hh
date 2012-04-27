f386ab58e84b698848392ebcc8957b3e326b338d
Milosz Derezynski <internalerror@gmail.com>
2012-04-27 14:28:02 +0200

    * Add vector_compare.hh which provides bool MPX::vector_compare<std::vector<T>>(a,b) with O(n) complexity

---
+++ include/mpx/algorithm/vector_compare.hh
@@ -0,0 +1,30 @@
+#ifndef MPX_VECTOR_COMPARE_HH
+#define MPX_VECTOR_COMPARE_HH
+
+#include <vector>
+
+namespace MPX
+{
+    template <typename T>
+    bool
+    vector_compare(const std::vector<T>& a, const std::vector<T>& b)
+    {
+	std::vector<T>::iterator i_a = a.begin() ;
+	std::vector<T>::iterator i_b = b.begin() ;
+
+	while( i_a != a.end() && i_b != b.end() )
+	{
+	    if( *i_a != *i_b )
+	    {
+		return false ;
+	    }
+
+	    ++i_a ;
+	    ++i_b ;
+	}
+
+	return true ;
+    }
+}
+
+#endif
